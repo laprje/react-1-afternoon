89c68bc36d7c5e12d63068b4d5e42bdb47ec19a6
L J <laprje@gmail.com>
2019-10-11 16:51:47 -0600

    made progress on FilterObject

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,9 +1,39 @@
 import React, {Component} from 'react';
 
 class FilterObject extends Component {
+    constructor() {
+        super();
+        this.setState = {
+            unfilteredArray: [
+                {age: 19, hairColor: brown, height: "Six Two"},
+                {age: 1, color: 'white', make: "Subaru", model: "STI"},
+                {age: 2, color: "white", make: "GMC", model: "Canyon"},
+                {color: 'green', name: 'grass'},
+                {name: "Price", age: 45, hairColor: "grey", height: "Six One"}
+            ],
+            userInput: '',
+            filteredArray: []
+
+            //finished here, you createed the constructor and finished all of the initial state requirements
+        }
+    }
     render() {
         return (
-            <p>FilterObject Component</p>
+            <div className="puzzleBox filterObjectPB">
+                <h4>Filter Object</h4>
+                <span className="puzzleText">
+
+                </span>
+                <input className="confirmationButton">
+
+                </input>
+                <button className="confirmationButton">
+
+                </button>
+                <span className="resultsBox filterObjectRB">
+
+                </span>
+            </div>
         )
     }
 }
