7f63a860e17a99ec44ed388ff3ac9a5962afe193
L J <laprje@gmail.com>
2019-10-12 19:51:58 -0600

    finished FilterObject

--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,41 +1,109 @@
-import React, {Component} from 'react';
+// import React, {Component} from 'react';
 
-class FilterObject extends Component {
-    constructor() {
-        super();
-        this.setState = {
-            unfilteredArray: [
-                {age: 19, hairColor: brown, height: "Six Two"},
-                {age: 1, color: 'white', make: "Subaru", model: "STI"},
-                {age: 2, color: "white", make: "GMC", model: "Canyon"},
-                {color: 'green', name: 'grass'},
-                {name: "Price", age: 45, hairColor: "grey", height: "Six One"}
-            ],
-            userInput: '',
-            filteredArray: []
+// class FilterObject extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             unfilteredArray: [
+//                 {age: 19, height: "Six Two", name: 'Lance'},
+//                 {age: 19, hairColor: 'blonde', name: "Paul"},
+//                 { age: 45, hairColor: "grey", height: "Six One"},
+//                 {name: "Kris", hairColor: 'black', height: 'Five Eight'}
+//             ],
+//             userInput: '',
+//             filteredArray: []
 
-            //finished here, you createed the constructor and finished all of the initial state requirements
-        }
+//             //finished here, you createed the constructor and finished all of the initial state requirements
+//         }
+//     }
+
+//     handleChange(val) {
+//         this.setState({userInput: val});
+//       }
+    
+//       filterArray(prop) {
+//         let unfilteredArray = this.state.unfilteredArray;
+//         let filteredArray = [];
+    
+//         for ( let i = 0; i < unfilteredArray.length; i++ ) {
+//           if ( filteredArray[i].hasOwnProperty(prop) ) {
+//             filteredArray.push(unfilteredArray[i]);
+//           }
+//         }
+    
+//         this.setState({ filteredArray: filteredArray });
+//       }
+
+//     render() {
+//         return (
+//             <div className="puzzleBox filterObjectPB">
+//                 <h4>Filter Object</h4>
+//                 <span className="puzzleText">
+//                 Original:
+//                 {JSON.stringify(this.state.unfilteredArray, null, 10)}
+//                 </span>
+//                 <input className="confirmationButton" onChange={(e) => this.handleChange(e.target.value) }>
+
+//                 </input>
+//                 <button className="confirmationButton" onClick={ () => this.filterArray(this.state.userInput) }>
+//                     Filter
+//                 </button>
+//                 <span className="resultsBox filterObjectRB"> Filtered:
+//                 {JSON.stringify(this.state.filteredArray, null, 10)}
+//                 </span>
+//             </div>
+//         )
+//     }
+// }
+
+// export default FilterObject;
+
+import React, { Component } from 'react';
+
+export default class FilterObject extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      unfiltered: [
+        {age: 19, height: "Six Two", name: 'Lance'},
+                        {age: 19, hairColor: 'blonde', name: "Paul"},
+                        { age: 45, hairColor: "grey", height: "Six One"},
+                        {name: "Kris", hairColor: 'black', height: 'Five Eight'}
+      ],
+
+      userInput: '',
+      filtered: []
     }
-    render() {
-        return (
-            <div className="puzzleBox filterObjectPB">
-                <h4>Filter Object</h4>
-                <span className="puzzleText">
+  }
 
-                </span>
-                <input className="confirmationButton">
+  handleChange(val) {
+    this.setState({ userInput: val });
+  }
 
-                </input>
-                <button className="confirmationButton">
+  filterEmployees(prop) {
+    let unfiltered = this.state.unfiltered;
+    let filtered = [];
 
-                </button>
-                <span className="resultsBox filterObjectRB">
-
-                </span>
-            </div>
-        )
+    for ( let i = 0; i < unfiltered.length; i++ ) {
+      if ( unfiltered[i].hasOwnProperty(prop) ) {
+        filtered.push(unfiltered[i]);
+      }
     }
+
+    this.setState({ filtered: filtered });
+  }
+
+  render() {
+    return (
+      <div className="puzzleBox filterObjectPB">
+        <h4> Filter Object </h4>
+        <span className="puzzleText"> Original: { JSON.stringify(this.state.unfiltered, null, 10) } </span>
+        <input className="inputLine" onChange={ (e) => this.handleChange(e.target.value) }></input>
+        <button className="confirmationButton" onClick={ () => this.filterEmployees(this.state.userInput) }> Filter </button>
+        <span className="resultsBox filterObjectRB"> Filtered: { JSON.stringify(this.state.filtered, null, 10) } </span>
+      </div>
+    )
+  }
 }
-
-export default FilterObject;
